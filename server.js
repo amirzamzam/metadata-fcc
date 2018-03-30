97cc71b24a4f4f12181be51674cb06197cbf560c
Glitch (metadata-fcc-amir) <none>
2018-03-30 08:04:14 +0000

    🍷📒 Checkpoint ./views/index.html:780437/200 ./server.js:780437/314

--- server.js
+++ server.js
@@ -6,23 +6,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
 const app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'))
 app.use(bodyParser.json()); 
 app.use(cors()); 
 
-
-
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + '/views/index.html')
 })
 
+app.post('/upload', upload.single('file'), function(req,res,next){
+
+return res.json(req.file);
+});
 
 
 // listen for requests :)
